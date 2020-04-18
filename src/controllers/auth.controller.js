52623ea1e598998b150900dc213d23126febe263
Ismael David Funes <ismafunes66@gmail.com>
2020-04-18 18:37:38 -0300

    avisale

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,37 +1,8 @@
 
 const jwt = require('jsonwebtoken');
-const moment = require('moment');
 const User = require('../models/user');
-const bcrypt = require('bcryptjs');
 
 const AuthController = {};
-// 2. Authentication Middleware
-AuthController.ensureAuthenticated = async (req, res, next) => {
-    if (!req.headers.authorization) {
-        return res.status(401).send({ error: 'TokenMissing' });
-    }
-    const token = req.headers.authorization.split(' ')[1];
-
-    let payload = null;
-    try {
-        payload = jwt.decode(token, config.TOKEN_SECRET);
-    }
-    catch (err) {
-        return res.status(401).send({ error: "TokenInvalid" });
-    }
-
-    if (payload.exp <= moment().unix()) {
-        return res.status(401).send({ error: 'TokenExpired' });
-    }
-    // check if the user exists
-    const person = await User.findById(payload.sub)
-    if (!person) {
-        return res.status(401).send({ error: 'PersonNotFound' });
-    } else {
-        req.user = payload.sub;
-        next();
-    }
-};
 
 AuthController.signup = async (req, res, next) => {
     try {
