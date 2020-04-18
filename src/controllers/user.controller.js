909f8b81155c52306c2ce6d72e81cb11117f02bc
Ismael David Funes <ismafunes66@gmail.com>
2020-04-18 18:40:29 -0300

    avisale

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,19 +2,5 @@ const User = require('../models/user');
 
 const UsersController = {};
 
-UsersController.register = async (req,res,next) => {
-    try {
-        const user = new User({
-            userName: req.body.userName,
-            names: req.body.names,
-            surName: req.body.surName,
-            password: req.body.password
-        });
-        const newUser = await user.save();
-        return res.send(newUser);
-    } catch (error) {
-        next(error);
-    }
-};
 
 module.exports = UsersController;
